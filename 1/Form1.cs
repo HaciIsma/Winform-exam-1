d1a20fec5c326470ae294916b99685cf2ada483d
HaciIsma <haciismayilov5@outlook.com>
2020-01-24 03:37:02 +0400

    first commit

---
+++ 1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsForms_Exam_1
+{
+    public partial class Form1 : Form
+    {
+        static int numbers = default;
+        MouseEventArgs cord;
+        public Form1()
+        {
+            InitializeComponent();
+
+            MouseDown += Form1_MouseDown;
+            MouseUp += Form1_MouseUp;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            cord = e;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int X = default;
+            int Y = default;
+            numbers++;
+            Label label = new Label();
+            this.Controls.Add(label);
+            label.Text = numbers.ToString();
+            label.TextAlign = ContentAlignment.TopCenter;
+            label.AutoSize = false;
+
+            if (cord.Location.X < e.Location.X && cord.Location.Y < e.Location.Y)
+            {
+                label.Location = cord.Location;
+                X = e.Location.X - cord.Location.X;
+                Y = e.Location.Y - cord.Location.Y;
+                label.Size = new Size(X, Y);
+            }
+            else if (cord.Location.X > e.Location.X && cord.Location.Y > e.Location.Y)
+            {
+                label.Location = e.Location;
+                X = cord.Location.X - e.Location.X;
+                Y = cord.Location.Y - e.Location.Y;
+                label.Size = new Size(X, Y);
+            }
+            else if (cord.Location.X < e.Location.X && cord.Location.Y > e.Location.Y)
+            {
+                label.Location = new Point(cord.X, e.Y);
+                X = e.Location.X - cord.Location.X;
+                Y = cord.Location.Y - e.Location.Y;
+                label.Size = new Size(X, Y);
+            }
+            else if (cord.Location.X > e.Location.X && cord.Location.Y < e.Location.Y)
+            {
+                label.Location = new Point(e.X, cord.Y);
+                X = cord.Location.X - e.Location.X;
+                Y = e.Location.Y - cord.Location.Y;
+                label.Size = new Size(X, Y);
+            }
+
+            Random rand = new Random();
+            label.BackColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256));
+            this.Controls.Add(label);
+        }
+    }
+}
---
+++ 2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
+        private void label_MouseMove(object sender, MouseEventArgs e)
+        {
+            int maxRange = 700;
+            Random random = new Random();
+            Direction direction = (Direction)random.Next(0, 3);
+            Point point = new Point(label.Location.X, label.Location.Y);
+            int range = random.Next(0, maxRange);
+
+            switch (direction)
+            {
+                case Direction.Up:
+                    for (; point.Y > range; point.Y--)
+                    {
+                        if (point.Y <= 15)
+                            goto case Direction.Down;
+                        label.Location = point;
+                    }
+                    break;
+                case Direction.Down:
+                    for (; point.Y < range; point.Y++)
+                    {
+                        if (point.Y >= 500)
+                            goto case Direction.Up;
+                        label.Location = point;
+                    }
+                    break;
+                case Direction.Left:
+                    for (; point.X > range; point.X--)
+                    {
+                        if (point.X <= 15)
+                            goto case Direction.Right;
+                        label.Location = point;
+                    }
+                    break;
+                case Direction.Right:
+                    if (point.X >= 500)
+                        goto case Direction.Left;
+                    for (; point.X < range; point.X++)
+                    {
+                        label.Location = point;
+                    }
+                    break;
+            }
+        }
+    }
+}
